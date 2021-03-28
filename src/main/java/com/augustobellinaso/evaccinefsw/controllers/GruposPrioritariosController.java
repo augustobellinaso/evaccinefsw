8f1fd40a91e8a8e09e33f921db52e95cdca9aa77
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 14:47:07 -0300

    Implementando controller grupoPrioritario

--- src/main/java/com/augustobellinaso/evaccinefsw/controllers/GruposPrioritariosController.java
+++ src/main/java/com/augustobellinaso/evaccinefsw/controllers/GruposPrioritariosController.java
@@ -1,4 +1,27 @@
 package com.augustobellinaso.evaccinefsw.controllers;
 
+import com.augustobellinaso.evaccinefsw.domain.GruposPrioritarios;
+import com.augustobellinaso.evaccinefsw.repository.GruposPrioritariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public class GruposPrioritariosController {
+
+    @Autowired
+    private GruposPrioritariosRepository gruposPrioritariosRepository;
+
+    @GetMapping
+    public List<GruposPrioritarios> listarTodosGrupos(){
+        return gruposPrioritariosRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public GruposPrioritarios buscarGrupoPeloCodigo(@PathVariable Long codigo){
+        return gruposPrioritariosRepository.findById(codigo).orElse(null);
+    }
+
+
 }
