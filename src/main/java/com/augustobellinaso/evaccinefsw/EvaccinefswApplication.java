413cff351b9b5b4205b8ff1d79ff0d67c4ad8cbe
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 14:09:24 -0300

    Initial commit

---
+++ src/main/java/com/augustobellinaso/evaccinefsw/EvaccinefswApplication.java
@@ -0,0 +1,13 @@
+package com.augustobellinaso.evaccinefsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaccinefswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaccinefswApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/augustobellinaso/evaccinefsw/EvaccinefswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustobellinaso.evaccinefsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaccinefswApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
