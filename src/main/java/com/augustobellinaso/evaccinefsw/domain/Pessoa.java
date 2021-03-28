2f73bcf42fcdf00e8854aa7e2ee44a0fcb25384c
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 14:17:09 -0300

    Criando as classes de Domínio da aplicação

---
+++ src/main/java/com/augustobellinaso/evaccinefsw/domain/Pessoa.java
@@ -0,0 +1,35 @@
+package com.augustobellinaso.evaccinefsw.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String nome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private String email;
+
+    private Integer idade;
+
+    private String dataNascimento;
+
+
+}
