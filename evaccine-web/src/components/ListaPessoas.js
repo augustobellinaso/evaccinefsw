de87acacd4c03288887235aed922091ebb16a513
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 15:25:02 -0300

    Inserindo a tabela de visualização

--- evaccine-web/src/components/ListaPessoas.js
+++ evaccine-web/src/components/ListaPessoas.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
+import {Table} from "antd";
+
+const {Column} = Table;
 
 export default class ListaPessoas extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            pessoas: [],
+            message: null
         }
     }
 
     render(){
         return(
-            <div>
+            <div className="container">
+                <h2>PESSOAS CADASTRADAS</h2>
+                <div className="container">
+                    <Table dataSource={this.state.pessoas}>
+                        <Column title="NOME" dataIndex="nome" key="nome"/>
+                        <Column title="CPF" dataIndex="cpf" key="cpf"/>
+                        <Column title="TELEFONE" dataIndex="telefone" key="telefone"/>
+                        <Column title="EMAIL" dataIndex="email" key="email"/>
+                        <Column title="IDADE" dataIndex="idade" key="idade"/>
+                        <Column title="DATA DE NASCIMENTO" dataIndex="dataNascimento" key="dataNascimento"/>
+                        <Column title="VACINADA" dataIndex="isVacinada" key="isVacinada"/>
+                        <Column title="ATUALIZAR" key="atualizar"/>
+                    </Table>
 
+                </div>
             </div>
         )
     }
