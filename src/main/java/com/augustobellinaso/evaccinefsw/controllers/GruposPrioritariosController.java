79957dc216d03ac3666cd60bee6199b0e0f5245a
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 14:49:43 -0300

    Anotação do controlador dos grupos

--- src/main/java/com/augustobellinaso/evaccinefsw/controllers/GruposPrioritariosController.java
+++ src/main/java/com/augustobellinaso/evaccinefsw/controllers/GruposPrioritariosController.java
@@ -5,9 +5,13 @@ import com.augustobellinaso.evaccinefsw.repository.GruposPrioritariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/grupos-prioritarios")
 public class GruposPrioritariosController {
 
     @Autowired
