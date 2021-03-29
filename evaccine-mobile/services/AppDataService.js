164830bd84f961102e20536f360a58f5f3fe8181
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 21:28:49 -0300

    Alterando a url da aplicação

--- evaccine-mobile/services/AppDataService.js
+++ evaccine-mobile/services/AppDataService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-//URL para utilizar com o Android Studio
-const API_URL = 'http://10.0.2.2:8080/';
+//URL para utilizar com o Android Studio 10.0.2.2
+//url para conexão local: 192.168.0.80
+const API_URL = 'http://192.168.0.80:8080/';
 
 class AppDataService{
     cadastrarPessoa(pessoa){
