35938f31795b56bc8353d98d0215bcb30d89bee4
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-03-28 21:16:32 -0300

    Fazendo o design da aplicação

--- evaccine-mobile/App.js
+++ evaccine-mobile/App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, Button} from 'react-native';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Cadastro eVaccine</Text>
+
+        <TextInput style={styles.input}
+                   placeholder="Nome" placeholderTextColor="#fff"/>
+        <TextInput style={styles.input}
+                   placeholder="CPF" placeholderTextColor="#fff"/>
+        <TextInput style={styles.input}
+                   placeholder="Telefone" placeholderTextColor="#fff"/>
+        <TextInput style={styles.input}
+                   placeholder="E-mail" placeholderTextColor="#fff"/>
+        <TextInput style={styles.input}
+                   placeholder="Idade" placeholderTextColor="#fff"/>
+        <TextInput style={styles.input}
+                   placeholder="Data de Nascimento" placeholderTextColor="#fff"/>
+        <TextInput style={styles.input}
+                   placeholder="Grupo Prioritário" placeholderTextColor="#fff"/>
+        <Button title="CADASTRAR"/>
+        <StatusBar style="auto"/>
+
+      </SafeAreaView>
   );
 }
 
@@ -15,7 +34,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    alignItems: 'center',
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#0D458D',
+    fontSize: 20,
+    color: '#fff',
+    backgroundColor: '#0D458D',
+
+  },
+
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 22
+  }
 });
